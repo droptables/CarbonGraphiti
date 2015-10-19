de4bfc711be3cb6fa87095785ae519a90f18325d
droptables <noah.corradin@gmail.com>
2015-10-18 20:04:09 -0700

    removing urls

--- src/Carbon-Graphiti.py
+++ src/Carbon-Graphiti.py
@@ -24,27 +24,25 @@ if __name__ == '__main__':
 	args=launch.get_args()
 	#load CB API
 	cb=launch.load_config_file(args.configfile)
-	linklist=['https://minotaurus.autodesk.com:8443/#analyze/00000aef-0000-2204-01d0-fdcfbafb6910/1','https://minotaurus.autodesk.com:8443/#analyze/00000aef-0000-2264-01d0-fdcfbb454514/1', 'https://minotaurus.autodesk.com:8443/#analyze/00000aef-0000-2194-01d0-fdcfc3d73761/1','https://minotaurus.autodesk.com:8443/#analyze/00000aef-0000-1e00-01d0-fdcfc471f184/1']
 	#Get process report for CB link
 
-	for link in linklist:
-		report=GetProcessReport.Run(cb,link)
-		#Create a timetable
-		timetable,timelist=CreateTimeTable.Run(report)
-		#Create time nodes to plot process activity on
-		CreateTimeNodes.Run(graph, timelist)
-		#Add modules loaded to time nodes
-		#AddModulesLoaded.Run(graph, timetable, report)
-		#Add file modifications
-		AddFileMods.Run(graph, timetable, report)
-		#Add Netcons
-		AddNetConns.Run(graph, timetable, report)
-		#Add registry mods
-		AddRegistryMods.Run(graph, timetable, report)
-		#Add Threat Intel from Bit9 on file modifications
-		AddFileModThreatIntel.Run(graph, timetable, report)
-		#Add Threat Intel from Bit9 on modules loaded
-		AddModulesLoadedThreatIntel.Run(graph, timetable, report)
+	report=GetProcessReport.Run(cb,link)
+	#Create a timetable
+	timetable,timelist=CreateTimeTable.Run(report)
+	#Create time nodes to plot process activity on
+	CreateTimeNodes.Run(graph, timelist)
+	#Add modules loaded to time nodes
+	#AddModulesLoaded.Run(graph, timetable, report)
+	#Add file modifications
+	AddFileMods.Run(graph, timetable, report)
+	#Add Netcons
+	AddNetConns.Run(graph, timetable, report)
+	#Add registry mods
+	AddRegistryMods.Run(graph, timetable, report)
+	#Add Threat Intel from Bit9 on file modifications
+	AddFileModThreatIntel.Run(graph, timetable, report)
+	#Add Threat Intel from Bit9 on modules loaded
+	AddModulesLoadedThreatIntel.Run(graph, timetable, report)
 
 
 	#Save output JSON file for OpenGraphiti
